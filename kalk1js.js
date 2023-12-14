27187d990023e23635f21b524ed0c6a6c753b3ee
Tom Li Dobnik <tomlidobnik1@gmail.com>
2023-12-14 21:30:20 +0100

    added styling

--- kalk1js.js
+++ kalk1js.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    input = "";
-    $("td").click(function () {
-        vnos = this.innerHTML;
-        if (vnos == "Del") {
-            input = "";
-            $("#display").html(input);
-        } else if (vnos == "pow" || vnos == "sqrt") {
-
-        } else {
-            input += this.innerHTML + " ";
-            $("#display").html(input);
-        }
-    });
+  input = "";
+  $("td").click(function () {
+    vnos = this.innerHTML;
+    if (vnos == "Del") {
+      input = "";
+      $("#display").html(input);
+    } else if (vnos == "pow" || vnos == "sqrt") {
+    } else {
+      input += this.innerHTML + " ";
+      $("#display").html(input);
+    }
+  });
 });
